033f218771a6308e1424fb877fe751a22f3ad749
LudvigsenAnders <anders.ludvigsen@specialisterne.dk>
2026-02-23 13:03:02 +0100

    modified:   delopgave_01/src/main.py 	modified:   delopgave_01/src/opg_01.ipynb

---
+++ delopgave_01/src/main.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+
+# Current file's directory
+current_dir = Path(__file__).parent
+
+parent_dir = current_dir.parent.parent
+
+file_path = parent_dir/'data'/'Navneliste.txt'
+
+print(file_path)
+
+
+with open(file_path, encoding="utf-8") as f:
+    read_data = f.read()
+
+names: list[str] = [name.strip() for name in read_data.split(',')]
+
+print(names)
+
+names_sorted_by_char = sorted(names, key=str.lower)
+names_sorted_by_len = sorted(names, key=len)
+
+
+my_char_dict = {}
+
+
+for name in names:
+    for char in name:
+        char = char.lower()
+        if char not in my_char_dict:
+            my_char_dict[char] = 1
+        else:
+            my_char_dict[char] += 1
+print(my_char_dict)
