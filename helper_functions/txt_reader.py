ca329bb636b6e86ddafddf905f7af42cd6aeea96
LudvigsenAnders <anders.ludvigsen@specialisterne.dk>
2026-02-23 13:19:49 +0100

    modified:   helper_functions/txt_reader.py

--- helper_functions/txt_reader.py
+++ helper_functions/txt_reader.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-
 # Current file's directory
 current_dir = Path(__file__).parent
 
@@ -11,21 +10,30 @@ file_path = parent_dir/'data'/'Navneliste.txt'
 print(file_path)
 
 
-with open(file_path, encoding="utf-8") as f:
-    read_data = f.read()
+def read_names_from_file(file_path: str) -> list[str]:
 
-names: list[str] = [name.strip() for name in read_data.split(',')]
-names_sorted_by_char = sorted(names, key=str.lower)
-names_sorted_by_len = sorted(names, key=len)
+    with open(file_path, encoding="utf-8") as f:
+        read_data = f.read()
 
-my_char_dict = {}
+    names: list[str] = [name.strip() for name in read_data.split(',')]
 
-for name in names:
-    for char in name:
-        char = char.lower()
-        if char not in my_char_dict:
-            my_char_dict[char] = 1
-        else:
-            my_char_dict[char] += 1
-print(my_char_dict)
-print(my_char_dict.get('a', 0))
+    return names
+
+def sort_names_by_char(names: list[str]) -> list[str]:
+    names_sorted_by_char = sorted(names, key=str.lower)
+    return names_sorted_by_char
+
+def sort_names_by_len(names: list[str]) -> list[str]:
+    names_sorted_by_len = sorted(names, key=len)
+    return names_sorted_by_len
+
+def count_characters(names: list[str]) -> dict[str, int]:
+    my_char_dict = {}
+    for name in names:
+        for char in name:
+            char = char.lower()
+            if char not in my_char_dict:
+                my_char_dict[char] = 1
+            else:
+                my_char_dict[char] += 1
+    return my_char_dict
