2ee11e7ade7b672cebca4db710b0c7f5132cd43c
LudvigsenAnders <anders.ludvigsen@specialisterne.dk>
2026-02-23 13:29:16 +0100

    modified:   helper_functions/txt_reader.py

--- helper_functions/txt_reader.py
+++ helper_functions/txt_reader.py
@@ -11,21 +11,21 @@ print(file_path)
 
 
 def read_names_from_file(file_path: str) -> list[str]:
-
     with open(file_path, encoding="utf-8") as f:
         read_data = f.read()
-
     names: list[str] = [name.strip() for name in read_data.split(',')]
-
     return names
+
 
 def sort_names_by_char(names: list[str]) -> list[str]:
     names_sorted_by_char = sorted(names, key=str.lower)
     return names_sorted_by_char
 
+
 def sort_names_by_len(names: list[str]) -> list[str]:
     names_sorted_by_len = sorted(names, key=len)
     return names_sorted_by_len
+
 
 def count_characters(names: list[str]) -> dict[str, int]:
     my_char_dict = {}
