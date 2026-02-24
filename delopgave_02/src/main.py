05a3bbefa2f780a0952d7fe2165bd4bed6dbdb71
LudvigsenAnders <anders.ludvigsen@specialisterne.dk>
2026-02-24 12:18:38 +0100

    modified:   delopgave_02/src/main.py

--- delopgave_02/src/main.py
+++ delopgave_02/src/main.py
@@ -1,1 +1,12 @@
+from pathlib import Path
+
 print("Hello from delopgave 02!")
+
+path = Path(__file__).resolve().parents[2]
+
+print(path)
+
+data_file = path / "data" / "app_log (logfil analyse) - random.txt"
+with open(data_file, "r") as f:
+    log_data = f.readlines()
+print(log_data[:5])
