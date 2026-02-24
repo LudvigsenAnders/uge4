e954272387718f3df658e7399cfee36a00cd2200
LudvigsenAnders <anders.ludvigsen@specialisterne.dk>
2026-02-24 12:27:26 +0100

    modified:   delopgave_02/src/main.py

--- delopgave_02/src/main.py
+++ delopgave_02/src/main.py
@@ -3,10 +3,10 @@ from pathlib import Path
 print("Hello from delopgave 02!")
 
 path = Path(__file__).resolve().parents[2]
-
 print(path)
 
 data_file = path / "data" / "app_log (logfil analyse) - random.txt"
+
 try:
     with open(data_file, "r") as f:
         log_data = f.readlines()
