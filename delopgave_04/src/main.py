b96810b18ba1f040dd3ab182d1b94c452ce409b2
LudvigsenAnders <anders.ludvigsen@specialisterne.dk>
2026-02-25 15:19:45 +0100

    new file:   delopgave_04/src/intermediate_01.ipynb 	modified:   delopgave_04/src/main.py

--- delopgave_04/src/main.py
+++ delopgave_04/src/main.py
@@ -5,17 +5,42 @@ from pathlib import Path
 path = Path(__file__).resolve().parents[2]
 print(path)
 
-source_file_01 = path / "data" / "source_data.csv"
-source_file_02 = path / "data" / "write_protect_source_data.csv"
+path_housing = path / "data" / "DKHousingPricesSample100k.csv"
+
 path_out = path / "data" / "csv_output"
 
 df = pd.read_csv(
-    source_file_01,
-    encoding="utf-8",
+    path_housing,
     sep=",",
     header=0,
     index_col=False,
     on_bad_lines='warn',
+    dtype={
+        "date": "string",
+        "quarter": "string",
+        "house_id": "int64",
+        "house_type": "string",
+        "sales_type": "string",
+        "year_build": "int64",
+        "purchase_price": "float64",
+        "%_change_between_offer_and_purchase": "float64",
+        "no_rooms": "int64",
+        "sqm": "float64",
+        "sqm_price": "float64",
+        "address": "string",
+        "zip_code": "int64",
+        "city": "string",
+        "area": "string",
+        "region": "string",
+        "%_nom_interest_rate%": float,
+        "%_dk_ann_infl_rate%": float,
+        "%_yield_on_mortgage_credit_bonds%": float
+    }
 )
 
-df.to_csv(path_out / "output.csv", index=False)
+df['date'] = pd.to_datetime(df['date'], errors='coerce', format='%Y-%m-%d').dt.date
+df['quarter'] = pd.PeriodIndex(df['quarter'], freq='Q')
+
+print(df.info())
+print(df.describe(include='all'))
+print(df.head(10))
